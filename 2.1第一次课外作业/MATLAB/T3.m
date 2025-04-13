717f4b5e861d01f8412fe7210fc11e813b82e548
俊霆 吕 <ytqxljt@outlook.com>
2025-04-13 20:44:46 +0800

    第一次

---
+++ 2.1第一次课外作业/MATLAB/T3.m
@@ -0,0 +1,12 @@
+omega = 1;
+U = 1.0;
+I = 2.0;
+u = @(t) sqrt(2)*U*sin(omega*t);
+i = @(t) sqrt(2)*I*sin(omega*t);
+p = @(t) U*I*sin(2*omega*t);
+
+fplot(u,[-2*pi,2*pi],"r-");
+hold on
+fplot(i,[-2*pi,2*pi],"g-");
+fplot(p,[-2*pi,2*pi],"b-");
+hold off
